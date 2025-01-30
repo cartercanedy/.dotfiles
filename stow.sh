9b1e583eeab1cc153a04d9c64430a63e1951e97c
Carter Canedy <cartercanedy42@gmail.com>
2025-01-30 11:39:42 -0800

    nvim cfg

--- stow.sh
+++ stow.sh
@@ -1,41 +1,29 @@
 #!/bin/sh -e
 
-if which stow -s; then
-  OS_TYPE=$(
-    OS_INFO=""
+if which stow -s 2>&1 > /dev/null; then
+  OS_INFO=""
 
-    if [ -f "/etc/os-release" ]; then
-      OS_INFO="/etc/os-release"
-    elif [ -f "/usr/lib/os-release" ]; then
-      OS_INFO="/etc/os-release"
-    else
-      echo ""
-      exit
-    fi
-
-    . "$OS_INFO"
-
-    OS_TYPE="$(echo "$ID_LIKE" | sed 's/.*\(debian|fedora\).*/\1/')"
-
-    if [ "$OS_TYPE" = "" ]; then
-      echo "$ID_LIKE"
-      exit 1
-    else
-      echo "$OS_TYPE"
-    fi
-  )
-
-  if ! [ "$?" = "0" ]; then
-    echo "Error: couldn't identify os: $OS_TYPE"
+  if [ -f "/etc/os-release" ]; then
+    OS_INFO="/etc/os-release"
+  elif [ -f "/usr/lib/os-release" ]; then
+    OS_INFO="/usr/lib/os-release"
+  else
+    echo "Unknown environment"
     exit 1
   fi
 
-  case "$OS_TYPE" in
-    debian)
+  . "$OS_INFO"
+
+  case "$ID_LIKE" in
+    *debian*)
       sudo apt install stow -y
       ;;
-    fedora)
+    *fedora*)
       sudo dnf -y install stow
+      ;;
+    *)
+      echo "Unknown OS kind: $ID_LIKE"
+      exit 1
       ;;
   esac
 fi
